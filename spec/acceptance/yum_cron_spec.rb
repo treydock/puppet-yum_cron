f12752c2a82586bcc94daf495da63083ff5974c7
Trey Dockendorf <treydock@gmail.com>
2015-10-02 15:32:48 -0500

    * Update acceptance tests to verify EL7 configs * Attempt to run docker based acceptance tests from travis-ci * Update nodesets for vagrant tests

--- spec/acceptance/yum_cron_spec.rb
+++ spec/acceptance/yum_cron_spec.rb
@@ -6,7 +6,7 @@ describe 'yum_cron class:' do
       pp = "class { 'yum_cron': }"
 
       apply_manifest(pp, :catch_failures => true)
-      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+      apply_manifest(pp, :catch_changes => true)
     end
 
     describe package('yum-cron') do
@@ -17,26 +17,50 @@ describe 'yum_cron class:' do
       it { should be_enabled }
       it { should be_running }
     end
-  
-    describe file('/etc/sysconfig/yum-cron') do
-      it { should be_file }
-      it { should be_owned_by 'root' }
-      it { should be_grouped_into 'root' }
-      it { should be_mode 644 }
-      its(:content) { should match /^CHECK_ONLY=yes$/ }
-      its(:content) { should match /^DOWNLOAD_ONLY=no$/ }
-      if fact('operatingsystemmajrelease') >= '6'
-        its(:content) { should match /^YUM_PARAMETER=$/ }
-        its(:content) { should match /^CHECK_FIRST=no$/ }
-        its(:content) { should match /^ERROR_LEVEL=0$/ }
-        its(:content) { should match /^DEBUG_LEVEL=0$/ }
-        its(:content) { should match /^RANDOMWAIT=60$/ }
-        its(:content) { should match /^MAILTO=root$/ }
-        its(:content) { should match /^SYSTEMNAME=#{fact('fqdn')}$/ }
-        its(:content) { should match /^DAYS_OF_WEEK=0123456$/ }
-        its(:content) { should match /^CLEANDAY=0$/ }
-        its(:content) { should match /^SERVICE_WAITS=yes$/ }
-        its(:content) { should match /^SERVICE_WAIT_TIME=300$/ }
+
+    if fact('operatingsystemmajrelease') <= '6'
+      describe file('/etc/yum/yum-cron.conf') do
+        it { should_not be_file }
+      end
+      describe file('/etc/sysconfig/yum-cron') do
+        it { should be_file }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_mode 644 }
+        its(:content) { should match /^CHECK_ONLY=yes$/ }
+        its(:content) { should match /^DOWNLOAD_ONLY=no$/ }
+        if fact('operatingsystemmajrelease') >= '6'
+          its(:content) { should match /^YUM_PARAMETER=$/ }
+          its(:content) { should match /^CHECK_FIRST=no$/ }
+          its(:content) { should match /^ERROR_LEVEL=0$/ }
+          its(:content) { should match /^DEBUG_LEVEL=0$/ }
+          its(:content) { should match /^RANDOMWAIT=60$/ }
+          its(:content) { should match /^MAILTO=root$/ }
+          its(:content) { should match /^SYSTEMNAME=#{fact('fqdn')}$/ }
+          its(:content) { should match /^DAYS_OF_WEEK=0123456$/ }
+          its(:content) { should match /^CLEANDAY=0$/ }
+          its(:content) { should match /^SERVICE_WAITS=yes$/ }
+          its(:content) { should match /^SERVICE_WAIT_TIME=300$/ }
+        end
+      end
+    else
+      describe file('/etc/sysconfig/yum-cron') do
+        it { should_not be_file }
+      end
+      describe file('/etc/yum/yum-cron.conf') do
+        it { should be_file }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_mode 644 }
+        its(:content) { should match /^update_cmd = default$/ }
+        its(:content) { should match /^update_messages = yes$/ }
+        its(:content) { should match /^download_updates = yes$/ }
+        its(:content) { should match /^apply_updates = no$/ }
+        its(:content) { should match /^random_sleep = 360$/ }
+        its(:content) { should match /^system_name = #{fact('fqdn')}$/ }
+        its(:content) { should match /^email_to = root$/ }
+        its(:content) { should match /^email_host = localhost$/ }
+        its(:content) { should match /^debuglevel = -2$/ }
       end
     end
 
@@ -57,7 +81,7 @@ describe 'yum_cron class:' do
         pp = "class { 'yum_cron': yum_autoupdate_ensure => 'absent' }"
 
         apply_manifest(pp, :catch_failures => true)
-        expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+        apply_manifest(pp, :catch_changes => true)
       end
         
       describe package('yum-autoupdate') do
