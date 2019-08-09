41ea03c521d1da304240d3f0d80ebf2cf4a43c03
Trey Dockendorf <tdockendorf@osc.edu>
2019-08-09 14:23:40 -0400

    Rubocop fixes

--- spec/unit/puppet/type/yum_cron_config_spec.rb
+++ spec/unit/puppet/type/yum_cron_config_spec.rb
@@ -2,80 +2,80 @@ require 'puppet'
 require 'puppet/type/yum_cron_config'
 
 describe 'Puppet::Type.type(:yum_cron_config)' do
-  before :each do
-    @yum_cron_config = Puppet::Type.type(:yum_cron_config).new(:name => 'vars/foo', :value => 'bar')
+  let(:yum_cron_config) do
+    Puppet::Type.type(:yum_cron_config).new(name: 'vars/foo', value: 'bar')
   end
 
-  it 'should require a name' do
+  it 'requires a name' do
     expect {
       Puppet::Type.type(:yum_cron_config).new({})
     }.to raise_error(Puppet::Error, 'Title or name must be provided')
   end
 
-  it 'should not expect a name with whitespace' do
+  it 'does not expect a name with whitespace' do
     expect {
-      Puppet::Type.type(:yum_cron_config).new(:name => 'f oo')
-    }.to raise_error(Puppet::Error, /Invalid yum_cron_config/)
+      Puppet::Type.type(:yum_cron_config).new(name: 'f oo')
+    }.to raise_error(Puppet::Error, %r{Invalid yum_cron_config})
   end
 
-  it 'should fail when there is no section' do
+  it 'fails when there is no section' do
     expect {
-      Puppet::Type.type(:yum_cron_config).new(:name => 'foo')
-    }.to raise_error(Puppet::Error, /Invalid yum_cron_config/)
+      Puppet::Type.type(:yum_cron_config).new(name: 'foo')
+    }.to raise_error(Puppet::Error, %r{Invalid yum_cron_config})
   end
 
-  it 'should not require a value when ensure is absent' do
-    Puppet::Type.type(:yum_cron_config).new(:name => 'vars/foo', :ensure => :absent)
+  it 'does not require a value when ensure is absent' do
+    Puppet::Type.type(:yum_cron_config).new(name: 'vars/foo', ensure: :absent)
   end
 
-  it 'should require a value when ensure is present' do
+  it 'requires a value when ensure is present' do
     expect {
-      Puppet::Type.type(:yum_cron_config).new(:name => 'vars/foo', :ensure => :present)
-    }.to raise_error(Puppet::Error, /Property value must be set/)
+      Puppet::Type.type(:yum_cron_config).new(name: 'vars/foo', ensure: :present)
+    }.to raise_error(Puppet::Error, %r{Property value must be set})
   end
 
-  it 'should accept a valid value' do
-    @yum_cron_config[:value] = 'bar'
-    expect(@yum_cron_config[:value]).to eql('bar')
+  it 'accepts a valid value' do
+    yum_cron_config[:value] = 'bar'
+    expect(yum_cron_config[:value]).to eql('bar')
   end
 
-  it 'should not accept a value with whitespace' do
-    @yum_cron_config[:value] = 'b ar'
-    expect(@yum_cron_config[:value]).to eql('b ar')
+  it 'does not accept a value with whitespace' do
+    yum_cron_config[:value] = 'b ar'
+    expect(yum_cron_config[:value]).to eql('b ar')
   end
 
-  it 'should accept valid ensure values' do
-    @yum_cron_config[:ensure] = :present
-    expect(@yum_cron_config[:ensure]).to eql(:present)
-    @yum_cron_config[:ensure] = :absent
-    expect(@yum_cron_config[:ensure]).to eql(:absent)
+  it 'accepts valid ensure values' do
+    yum_cron_config[:ensure] = :present
+    expect(yum_cron_config[:ensure]).to be(:present)
+    yum_cron_config[:ensure] = :absent
+    expect(yum_cron_config[:ensure]).to be(:absent)
   end
 
-  it 'should not accept invalid ensure values' do
+  it 'does not accept invalid ensure values' do
     expect {
-      @yum_cron_config[:ensure] = :latest
-    }.to raise_error(Puppet::Error, /Invalid value/)
+      yum_cron_config[:ensure] = :latest
+    }.to raise_error(Puppet::Error, %r{Invalid value})
   end
 
   describe 'autorequire resources' do
-    it 'should autorequire File[/etc/yum/yum-cron.conf]' do
-      conf = Puppet::Type.type(:file).new(:name => '/etc/yum/yum-cron.conf')
+    it 'autorequires File[/etc/yum/yum-cron.conf]' do
+      conf = Puppet::Type.type(:file).new(name: '/etc/yum/yum-cron.conf')
       catalog = Puppet::Resource::Catalog.new
-      catalog.add_resource @yum_cron_config
+      catalog.add_resource yum_cron_config
       catalog.add_resource conf
-      rel = @yum_cron_config.autorequire[0]
+      rel = yum_cron_config.autorequire[0]
       expect(rel.source.ref).to eql(conf.ref)
-      expect(rel.target.ref).to eql(@yum_cron_config.ref)
+      expect(rel.target.ref).to eql(yum_cron_config.ref)
     end
 
-    it 'should autorequire Package[yum-cron]' do
-      conf = Puppet::Type.type(:package).new(:name => 'yum-cron')
+    it 'autorequires Package[yum-cron]' do
+      conf = Puppet::Type.type(:package).new(name: 'yum-cron')
       catalog = Puppet::Resource::Catalog.new
-      catalog.add_resource @yum_cron_config
+      catalog.add_resource yum_cron_config
       catalog.add_resource conf
-      rel = @yum_cron_config.autorequire[0]
+      rel = yum_cron_config.autorequire[0]
       expect(rel.source.ref).to eql(conf.ref)
-      expect(rel.target.ref).to eql(@yum_cron_config.ref)
+      expect(rel.target.ref).to eql(yum_cron_config.ref)
     end
   end
 end
