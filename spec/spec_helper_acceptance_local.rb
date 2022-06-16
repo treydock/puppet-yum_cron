17a0ce7bb7291176440022c7ec4729d4782591f6
Trey Dockendorf <tdockendorf@osc.edu>
2022-06-16 09:13:44 -0400

    Update docs for EL9 Ensure EL8 and EL9 are treated the same (now default behavior) Update acceptance tests to also work with EL9

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -1,14 +1,14 @@
 def defaults(release, key)
   d = {
-    8 => {
-      'package' => 'dnf-automatic',
-      'config'  => '/etc/dnf/automatic.conf',
-      'service' => 'dnf-automatic.timer',
-    },
-    'default' => {
+    7 => {
       'package' => 'yum-cron',
       'config'  => '/etc/yum/yum-cron.conf',
       'service' => 'yum-cron',
+    },
+    'default' => {
+      'package' => 'dnf-automatic',
+      'config'  => '/etc/dnf/automatic.conf',
+      'service' => 'dnf-automatic.timer',
     },
   }
   d.fetch(release, d['default'])[key]
